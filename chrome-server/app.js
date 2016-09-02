a859335fbd14d3251fcfdaf22e62bcd27f69663e
corbinmuraro <corbinmuraro@gmail.com>
2016-09-02 22:58:28 +0000

    Change server from web socket to HTTP Post

--- chrome-server/app.js
+++ chrome-server/app.js
@@ -8,39 +8,70 @@ var client = new MsTranslator({
   client_secret: config.client_secret
 }, true);
 
-// var params = {
-//   text: 'How are you?',
-//   from: 'en',
-//   to: 'zh-CHS'
-// };
 
-// Don't worry about access token, it will be auto-generated if needed.
-// client.translate(params, function(err, data) {
-//   console.log(data);
-// });
+var express = require('express');
+var bodyParser = require('body-parser');
 
-var io = require('socket.io').listen(8888);
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-io.sockets.on('connection', function(socket) {
+app.post('/', function (req, res) {
+    res.setHeader('Content-Type', 'application/json');
 
-	socket.on('pre-translate', function(data) {
+    var preTranslate = req.body.content;
+    console.log(preTranslate);
 
-		console.log(data.value + "  " + data.from + "  " + data.to);
+    var params = {
+      text: preTranslate,
+      from: 'en',
+      to: 'zh-CHS'
+    };
 
-		var params = {
-			text: data.value,
-			from: data.from,
-			to: data.to
-		};
-
-		var translatedText = "";
-
-		client.translate(params, function(err, data) {
-			console.log(data);
-
-			socket.emit('post-translate', {
-	    		value: data
-	   		});
-		});
-	});
+    // Don't worry about access token, it will be auto-generated if needed.
+    client.translate(params, function(err, postTranslate) {
+        console.log(postTranslate);
+        res.send(JSON.stringify({
+            content: postTranslate
+        }));
+    });
 });
+
+app.listen(8888, function () {
+  console.log('Example app listening on port 8888!');
+});
+
+
+
+
+
+
+
+
+// . • * ° SOCKET MAGIC ° * • . //
+
+// var io = require('socket.io').listen(8888);
+
+// io.sockets.on('connection', function(socket) {
+
+// 	socket.on('pre-translate', function(data) {
+
+// 		console.log(data.value + "  " + data.from + "  " + data.to);
+
+// 		var params = {
+// 			text: data.value,
+// 			from: data.from,
+// 			to: data.to
+// 		};
+
+// 		var translatedText = "";
+
+// 		client.translate(params, function(err, data) {
+// 			console.log(data);
+
+// 			socket.emit('post-translate', {
+// 	    		value: data
+// 	   		});
+// 		});
+// 	});
+// });
