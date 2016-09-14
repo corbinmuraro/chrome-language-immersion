033121ab243b1fbd738b5db4973deb34fe62ea86
Corbin Muraro <corbinmuraro@gmail.com>
2016-09-13 17:08:58 -0700

    Style / add funcitionality to popup
    Add options button and header
    Add list of untranslated / translated pairs
    TODO: add 'disable for this domain' button to popup and 'disabled domain manager' (add/remove) to options

--- js/popup.js
+++ js/popup.js
@@ -1,10 +1,14 @@
-
-function openOptions() {
-	chrome.runtime.openOptionsPage();
-}
+$(document).ready(function() {
+	$('#options').click( function() {
+		chrome.runtime.openOptionsPage();
+	});
+});
 
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 		chrome.tabs.sendMessage(tabs[0].id, {type: "getCount"}, function(array) {
+			if ((typeof array == "undefined" || !(array instanceof Array)))
+				return;
+
 			if (array.length == 1)
 				$('#alert').append(array.length + " phrase translated");
 			else
@@ -14,11 +18,13 @@ chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 			for (var i = 0; i < array.length; i++)
 			{    
 				var html = 	'<div class="phraseGroup">' + 
-								'<div>' + array[i].untranslated + '</div>' +
-								'<div>' + array[i].translated + '</div>' +
+								'<div class="col1">' + array[i].untranslated + '</div>' +
+								'<div class="padding"></div>' + 
+								'<div class="col2">' + array[i].translated + '</div>' +
 							'</div>';
 
 				$('.phraseList').append(html);
 			}
 		});
 	});
+
