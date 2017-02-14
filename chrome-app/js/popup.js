1d4692bcbe9bf008e3363b399b3ee7facbd590a0
Corbin Muraro <corbinmuraro@gmail.com>
2017-02-13 19:18:03 -0800

    Clean up sign in button

--- chrome-app/js/popup.js
+++ chrome-app/js/popup.js
@@ -15,23 +15,13 @@ function initApp() {
     if (user) {
       // User is signed in.
       var displayName = user.displayName;
-      var email = user.email;
-      var emailVerified = user.emailVerified;
-      var photoURL = user.photoURL;
-      var isAnonymous = user.isAnonymous;
-      var uid = user.uid;
-      var providerData = user.providerData;
       // [START_EXCLUDE]
       document.getElementById('quickstart-button').textContent = 'Sign out';
-      document.getElementById('quickstart-sign-in-status').textContent = 'Signed in';
-      document.getElementById('quickstart-account-details').textContent = JSON.stringify(user, null, '  ');
       // [END_EXCLUDE]
     } else {
       // Let's try to get a Google auth token programmatically.
       // [START_EXCLUDE]
       document.getElementById('quickstart-button').textContent = 'Sign-in with Google';
-      document.getElementById('quickstart-sign-in-status').textContent = 'Signed out';
-      document.getElementById('quickstart-account-details').textContent = 'null';
       // [END_EXCLUDE]
     }
     document.getElementById('quickstart-button').disabled = false;
